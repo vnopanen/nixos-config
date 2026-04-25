a029504a9f5ef51aa541cc168569d7cea26f354e
vnopanen <nopanenv@gmail.com>
2026-04-25 13:24:36 +0300

    update caches

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   description = "NixOS Flake Configuration";
 
   nixConfig = {
-    extra-substituters = [
+    substituters = [
+      "https://cache.nixos.org/"
       "https://nixos-raspberrypi.cachix.org"
       "https://nix-community.cachix.org"
     ];
-    extra-trusted-public-keys = [
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
